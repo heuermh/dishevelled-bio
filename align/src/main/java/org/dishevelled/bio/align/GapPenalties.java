7d7f75dd7a450fa65aaa96e4a16403f20f7c1991
Michael L Heuer <heuermh@acm.org>
2016-02-03 11:08:23 -0600

    update copyright years to include 2016

--- align/src/main/java/org/dishevelled/bio/align/GapPenalties.java
+++ align/src/main/java/org/dishevelled/bio/align/GapPenalties.java
@@ -1,7 +1,7 @@
 /*
 
     dsh-bio-align  Sequence alignment.
-    Copyright (c) 2013-2015 held jointly by the individual authors.
+    Copyright (c) 2013-2016 held jointly by the individual authors.
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Lesser General Public License as published
