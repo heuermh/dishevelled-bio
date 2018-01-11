a5393c43b1179e719757faf2958bd2e72a804abe
Michael L Heuer <heuermh@acm.org>
2018-01-11 12:33:30 -0600

    update copyright years to include 2018

--- convert-htsjdk/src/test/java/org/dishevelled/bio/convert/htsjdk/VcfFilterHeaderLineToVCFFilterHeaderLineTest.java
+++ convert-htsjdk/src/test/java/org/dishevelled/bio/convert/htsjdk/VcfFilterHeaderLineToVCFFilterHeaderLineTest.java
@@ -1,7 +1,7 @@
 /*
 
     dsh-bio-convert-htsjdk  Convert between dishevelled and htsjdk data models.
-    Copyright (c) 2013-2017 held jointly by the individual authors.
+    Copyright (c) 2013-2018 held jointly by the individual authors.
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Lesser General Public License as published
