2d8ac85b463011d4b5b092fcfd2430a4dfda28b5
Michael L Heuer <heuermh@acm.org>
2018-01-22 13:29:26 -0500

    separate interleave and disinterleave benchmarks

---
+++ benchmarks/src/main/java/org/dishevelled/bio/benchmarks/DisinterleaveFastqBenchmarks.java
@@ -0,0 +1,75 @@
+/*
+
+    dsh-bio-benchmarks.  Benchmarks.
+    Copyright (c) 2013-2018 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.bio.benchmarks;
+
+import java.io.File;
+
+import com.google.common.io.Files;
+import com.google.common.io.Resources;
+
+import org.dishevelled.bio.tools.DisinterleaveFastq;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+
+/**
+ * DisinterleaveFastq benchmarks.
+ *
+ * @author  Michael Heuer
+ */
+@State(Scope.Thread)
+public class DisinterleaveFastqBenchmarks {
+    private File pairedFile;
+    private File firstFastqFile;
+    private File secondFastqFile;
+
+    @Setup(Level.Invocation)
+    public void setUp() throws Exception {
+        pairedFile = File.createTempFile("disinterleaveFastqBenchmarks", ".ifq.gz");
+        firstFastqFile = File.createTempFile("disinterleaveFastqBenchmarks", ".fq.gz");
+        secondFastqFile = File.createTempFile("disinterleaveFastqBenchmarks", ".fq.gz");
+
+        copyResource("NIST7035_TAAGGCGA_L002.20000.0.ifq.gz", pairedFile);
+    }
+
+    @TearDown(Level.Invocation)
+    public void tearDown() {
+        pairedFile.delete();
+        firstFastqFile.delete();
+        secondFastqFile.delete();
+    }
+
+    @Benchmark
+    public void disinterleaveFastq() throws Exception {
+        new DisinterleaveFastq(pairedFile, null, firstFastqFile, secondFastqFile).call();
+    }
+
+    private static void copyResource(final String name, final File file) throws Exception {
+        Files.write(Resources.toByteArray(DisinterleaveFastqBenchmarks.class.getResource(name)), file);
+    }
+}
---
+++ benchmarks/src/main/java/org/dishevelled/bio/benchmarks/InterleaveFastqBenchmarks.java
@@ -0,0 +1,79 @@
+/*
+
+    dsh-bio-benchmarks.  Benchmarks.
+    Copyright (c) 2013-2018 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.bio.benchmarks;
+
+import java.io.File;
+
+import com.google.common.io.Files;
+import com.google.common.io.Resources;
+
+import org.dishevelled.bio.tools.InterleaveFastq;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+
+/**
+ * InterleaveFastq benchmarks.
+ *
+ * @author  Michael Heuer
+ */
+@State(Scope.Thread)
+public class InterleaveFastqBenchmarks {
+    private File firstFastqFile;
+    private File secondFastqFile;
+    private File pairedFile;
+    private File unpairedFile;
+
+    @Setup(Level.Invocation)
+    public void setUp() throws Exception {
+        firstFastqFile = File.createTempFile("interleaveFastqBenchmarks", ".fq.gz");
+        secondFastqFile = File.createTempFile("interleaveFastqBenchmarks", ".fq.gz");
+        pairedFile = File.createTempFile("interleaveFastqBenchmarks", ".ifq.gz");
+        unpairedFile = File.createTempFile("interleaveFastqBenchmarks", ".fq.gz");
+
+        copyResource("NIST7035_TAAGGCGA_L002_R1_001_trimmed.10000.0.fq.gz", firstFastqFile);
+        copyResource("NIST7035_TAAGGCGA_L002_R2_001_trimmed.10000.0.fq.gz", secondFastqFile);
+    }
+
+    @TearDown(Level.Invocation)
+    public void tearDown() {
+        firstFastqFile.delete();
+        secondFastqFile.delete();
+        pairedFile.delete();
+        unpairedFile.delete();
+    }
+
+    @Benchmark
+    public void interleaveFastq() throws Exception {
+        new InterleaveFastq(firstFastqFile, secondFastqFile, pairedFile, unpairedFile).call();
+    }
+
+    private static void copyResource(final String name, final File file) throws Exception {
+        Files.write(Resources.toByteArray(InterleaveFastqBenchmarks.class.getResource(name)), file);
+    }
+}
