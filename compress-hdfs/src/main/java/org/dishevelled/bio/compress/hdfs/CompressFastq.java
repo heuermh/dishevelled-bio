e2864879ad696ca4bb7a351610defb27c7236876
Michael L Heuer <heuermh@acm.org>
2018-11-05 16:17:40 -0600

    adding compress-fastq tool

--- compress-hdfs/src/main/java/org/dishevelled/bio/compress/hdfs/CompressFastq.java
+++ compress-hdfs/src/main/java/org/dishevelled/bio/compress/hdfs/CompressFastq.java
@@ -32,8 +32,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import java.util.concurrent.Callable;
-
 import org.apache.hadoop.conf.Configuration;
 
 import org.biojava.bio.program.fastq.Fastq;
