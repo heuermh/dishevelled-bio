13b1943d4336451f8d042f6ba169076d60e2f78e
Michael L Heuer <heuermh@acm.org>
2020-09-24 14:07:55 -0500

    adding tools for PAF format

---
+++ tools/src/main/java/org/dishevelled/bio/tools/FilterPaf.java
@@ -0,0 +1,299 @@
+/*
+
+    dsh-bio-tools  Command line tools.
+    Copyright (c) 2013-2020 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.bio.tools;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import static org.dishevelled.compress.Readers.reader;
+import static org.dishevelled.compress.Writers.writer;
+
+import java.io.File;
+import java.io.PrintWriter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.script.Compilable;
+import javax.script.CompiledScript;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Range;
+
+import org.dishevelled.bio.range.Ranges;
+
+import org.dishevelled.bio.alignment.paf.PafReader;
+import org.dishevelled.bio.alignment.paf.PafRecord;
+import org.dishevelled.bio.alignment.paf.PafWriter;
+import org.dishevelled.bio.alignment.paf.PafStreamAdapter;
+
+import org.dishevelled.commandline.ArgumentList;
+import org.dishevelled.commandline.CommandLine;
+import org.dishevelled.commandline.CommandLineParseException;
+import org.dishevelled.commandline.CommandLineParser;
+import org.dishevelled.commandline.Switch;
+import org.dishevelled.commandline.Usage;
+
+import org.dishevelled.commandline.argument.FileArgument;
+import org.dishevelled.commandline.argument.IntegerArgument;
+import org.dishevelled.commandline.argument.StringArgument;
+
+/**
+ * Filter alignments in PAF format.
+ *
+ * @since 1.4
+ * @author  Michael Heuer
+ */
+public final class FilterPaf extends AbstractFilter {
+    private final List<Filter> filters;
+    private final File inputPafFile;
+    private final File outputPafFile;
+    private static final String USAGE = "dsh-filter-paf --mapq 30 -i input.paf.bgz -o output.paf.bgz";
+
+
+    /**
+     * Filter alignments in PAF format.
+     *
+     * @param filters list of filters, must not be null
+     * @param inputPafFile input PAF file, if any
+     * @param outputPafFile output PAF file, if any
+     */
+    public FilterPaf(final List<Filter> filters, final File inputPafFile, final File outputPafFile) {
+        checkNotNull(filters);
+        this.filters = ImmutableList.copyOf(filters);
+        this.inputPafFile = inputPafFile;
+        this.outputPafFile = outputPafFile;
+    }
+
+
+    @Override
+    public Integer call() throws Exception {
+        PrintWriter writer = null;
+        try {
+            writer = writer(outputPafFile);
+
+            final PrintWriter w = writer;
+            PafReader.stream(reader(inputPafFile), new PafStreamAdapter() {
+                    @Override
+                    public void record(final PafRecord record) {
+                        // write out record
+                        boolean pass = true;
+                        for (Filter filter : filters) {
+                            pass &= filter.accept(record);
+                        }
+                        if (pass) {
+                            PafWriter.writeRecord(record, w);
+                        }
+                    }
+                });
+
+            return 0;
+        }
+        finally {
+            try {
+                writer.close();
+            }
+            catch (Exception e) {
+                // empty
+            }
+        }
+    }
+
+    /**
+     * Filter.
+     */
+    interface Filter {
+
+        /**
+         * Return true if the specified PAF record should be accepted by this filter.
+         *
+         * @param record PAF record
+         * @return true if the specified PAF record should be accepted by this filter
+         */
+        boolean accept(PafRecord record);
+    }
+
+    /**
+     * Range filter.
+     */
+    public static final class RangeFilter implements Filter {
+        /** Query name. */
+        private final String queryName;
+
+        /** Range. */
+        private final Range<Long> range;
+
+        /** Range format regular expression. */
+        private final Pattern RANGE = Pattern.compile("^(.*):([0-9]+)-([0-9]+)$");
+
+        /**
+         * Create a new range filter with the specified range format.
+         *
+         * @param value range format, must not be null
+         */
+        public RangeFilter(final String value) {
+            checkNotNull(value);
+            Matcher m = RANGE.matcher(value);
+            if (!m.matches()) {
+                throw new IllegalArgumentException("invalid range format, expected chrom:start-end in 0-based coordinates");
+            }
+            this.queryName = m.group(1);
+            long start = Long.parseLong(m.group(2));
+            long end = Long.parseLong(m.group(3));
+            this.range = Range.closedOpen(start, end);
+        }
+
+        @Override
+        public boolean accept(final PafRecord record) {
+            return queryName.equals(record.getQueryName()) && Ranges.intersect(range, Range.closedOpen(record.getQueryStart(), record.getQueryEnd()));
+        }
+    }
+
+    /**
+     * Mapping quality filter.
+     */
+    public static final class MappingQualityFilter implements Filter {
+        /** Mapping quality. */
+        private final int mappingQuality;
+
+        /**
+         * Create a new mapping quality score filter with the specified mapping quality.
+         *
+         * @param mappingQuality mapping quality
+         */
+        public MappingQualityFilter(final int mappingQuality) {
+            this.mappingQuality = mappingQuality;
+        }
+
+        @Override
+        public boolean accept(final PafRecord record) {
+            return record.getMappingQuality() >= mappingQuality;
+        }
+    }
+
+    /**
+     * Script filter.
+     */
+    public static final class ScriptFilter implements Filter {
+        /** Compiled script. */
+        private final CompiledScript compiledScript;
+
+        /**
+         * Create a new script filter with the specified script.
+         *
+         * @param script script
+         */
+        public ScriptFilter(final String script) {
+            ScriptEngine engine = createScriptEngine();
+            try {
+                Compilable compilable = (Compilable) engine;
+                compiledScript = compilable.compile("function test(r) { return (" + script + ") }\nvar result = test(r)");
+            }
+            catch (ScriptException e) {
+                throw new IllegalArgumentException("could not compile script, caught " + e.getMessage(), e);
+            }
+         }
+
+        @Override
+        public boolean accept(final PafRecord record) {
+            try {
+                compiledScript.getEngine().put("r", record);
+                compiledScript.eval();
+                return (Boolean) compiledScript.getEngine().get("result");
+            }
+            catch (ScriptException e) {
+                throw new RuntimeException("could not evaluate compiled script, caught " + e.getMessage(), e);
+            }
+        }
+    }
+
+    /**
+     * Main.
+     *
+     * @param args command line args
+     */
+    public static void main(final String[] args) {
+        Switch about = new Switch("a", "about", "display about message");
+        Switch help = new Switch("h", "help", "display help message");
+        StringArgument rangeFilter = new StringArgument("r", "range", "filter by range, specify as queryName:start-end in 0-based coordindates", false);
+        IntegerArgument mappingQualityFilter = new IntegerArgument("q", "mapping-quality", "filter by mapping quality", false);
+        StringArgument scriptFilter = new StringArgument("e", "script", "filter by script, eval against r", false);
+        FileArgument inputPafFile = new FileArgument("i", "input-paf-file", "input PAF file, default stdin", false);
+        FileArgument outputPafFile = new FileArgument("o", "output-paf-file", "output PAF file, default stdout", false);
+
+        ArgumentList arguments = new ArgumentList(about, help, rangeFilter, mappingQualityFilter, scriptFilter, inputPafFile, outputPafFile);
+        CommandLine commandLine = new CommandLine(args);
+
+        FilterPaf filterPaf = null;
+        try {
+            CommandLineParser.parse(commandLine, arguments);
+            if (about.wasFound()) {
+                About.about(System.out);
+                System.exit(0);
+            }
+            if (help.wasFound()) {
+                Usage.usage(USAGE, null, commandLine, arguments, System.out);
+                System.exit(0);
+            }
+            List<Filter> filters = new ArrayList<Filter>();
+            if (rangeFilter.wasFound()) {
+                filters.add(new RangeFilter(rangeFilter.getValue()));
+            }
+            if (mappingQualityFilter.wasFound()) {
+                filters.add(new MappingQualityFilter(mappingQualityFilter.getValue()));
+            }
+            if (scriptFilter.wasFound()) {
+                filters.add(new ScriptFilter(scriptFilter.getValue()));
+            }
+            filterPaf = new FilterPaf(filters, inputPafFile.getValue(), outputPafFile.getValue());
+        }
+        catch (CommandLineParseException e) {
+            if (about.wasFound()) {
+                About.about(System.out);
+                System.exit(0);
+            }
+            if (help.wasFound()) {
+                Usage.usage(USAGE, null, commandLine, arguments, System.out);
+                System.exit(0);
+            }
+            Usage.usage(USAGE, e, commandLine, arguments, System.err);
+            System.exit(-1);
+        }
+        catch (NullPointerException | IllegalArgumentException e) {
+            Usage.usage(USAGE, e, commandLine, arguments, System.err);
+            System.exit(-1);
+        }
+        try {
+            System.exit(filterPaf.call());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
---
+++ tools/src/main/java/org/dishevelled/bio/tools/SplitPaf.java
@@ -0,0 +1,245 @@
+/*
+
+    dsh-bio-tools  Command line tools.
+    Copyright (c) 2013-2020 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.dishevelled.bio.tools;
+
+import static org.dishevelled.compress.Readers.reader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.io.Files;
+
+import org.dishevelled.bio.alignment.paf.PafReader;
+import org.dishevelled.bio.alignment.paf.PafRecord;
+import org.dishevelled.bio.alignment.paf.PafStreamListener;
+import org.dishevelled.bio.alignment.paf.PafWriter;
+
+import org.dishevelled.commandline.ArgumentList;
+import org.dishevelled.commandline.CommandLine;
+import org.dishevelled.commandline.CommandLineParseException;
+import org.dishevelled.commandline.CommandLineParser;
+import org.dishevelled.commandline.Switch;
+import org.dishevelled.commandline.Usage;
+
+import org.dishevelled.commandline.argument.FileArgument;
+import org.dishevelled.commandline.argument.IntegerArgument;
+import org.dishevelled.commandline.argument.LongArgument;
+import org.dishevelled.commandline.argument.StringArgument;
+
+import org.dishevelled.compress.Compress;
+
+/**
+ * Split PAF files.
+ *
+ * @since 1.4
+ * @author  Michael Heuer
+ */
+public final class SplitPaf extends AbstractSplit {
+    private static final String USAGE = "dsh-split-paf -r 100 -i foo.paf.bgz";
+
+    /**
+     * Split PAF files.
+     *
+     * @param inputFile input file, if any
+     * @param bytes split the input file at next record after each n bytes, if any
+     * @param records split the input file after each n records, if any
+     * @param prefix output file prefix, must not be null
+     * @param suffix output file suffix, must not be null
+     */
+    public SplitPaf(final File inputFile, final Long bytes, final Long records, final String prefix, final String suffix) {
+        this(inputFile, bytes, records, prefix, -1, suffix);
+    }
+
+    /**
+     * Split PAF files.
+     *
+     * @param inputFile input file, if any
+     * @param bytes split the input file at next record after each n bytes, if any
+     * @param records split the input file after each n records, if any
+     * @param prefix output file prefix, must not be null
+     * @param leftPad left pad split index in output file name
+     * @param suffix output file suffix, must not be null
+     */
+    public SplitPaf(final File inputFile, final Long bytes, final Long records, final String prefix, final int leftPad, final String suffix) {
+        super(inputFile, bytes, records, prefix, leftPad, suffix);
+    }
+
+
+    @Override
+    public Integer call() throws Exception {
+        BufferedReader reader = null;
+        try {
+            reader = reader(inputFile);
+
+            PafReader.stream(reader, new PafStreamListener() {
+                    private long r = 0L;
+                    private int files = 0;
+                    private CountingWriter writer;
+
+                    @Override
+                    public void record(final PafRecord record) {
+                        if (writer == null) {
+                            writer = createCountingWriter(files);
+                        }
+                        PafWriter.writeRecord(record, writer.asPrintWriter());
+                        try {
+                            writer.flush();
+                        }
+                        catch (IOException e) {
+                            // ignore
+                        }
+                        r++;
+
+                        if (r >= records || writer.getCount() >= bytes) {
+                            r = 0L;
+                            files++;
+
+                            try {
+                                writer.close();
+                            }
+                            catch (Exception e) {
+                                // ignore
+                            }
+                            finally {
+                                writer = null;
+                            }
+                        }
+                    }
+                });
+            return 0;
+        }
+        finally {
+            try {
+                reader.close();
+            }
+            catch (Exception e) {
+                // ignore
+            }
+            closeWriters();
+        }
+    }
+
+    static final String getBaseName(final File file) {
+        String baseName = Files.getNameWithoutExtension(file.getName());
+        // trim trailing .paf if present
+        return baseName.endsWith(".paf") ? baseName.substring(0, baseName.length() - 4) : baseName;
+    }
+
+    static final String getFileExtensions(final File file) {
+        String baseName = Files.getNameWithoutExtension(file.getName());
+        String extension = Files.getFileExtension(file.getName());
+        // add .paf to extension if present
+        return baseName.endsWith(".paf") ? ".paf." + extension : "." + extension;
+    }
+
+    /**
+     * Main.
+     *
+     * @param args command line args
+     */
+    public static void main(final String[] args) {
+        Switch about = new Switch("a", "about", "display about message");
+        Switch help = new Switch("h", "help", "display help message");
+        FileArgument inputFile = new FileArgument("i", "input-file", "input PAF file, default stdin", false);
+        StringArgument bytes = new StringArgument("b", "bytes", "split input file at next record after each n bytes", false);
+        LongArgument records = new LongArgument("r", "records", "split input file after each n records", false);
+        StringArgument prefix = new StringArgument("p", "prefix", "output file prefix", false);
+        IntegerArgument leftPad = new IntegerArgument("d", "left-pad", "left pad split index in output file name", false);
+        StringArgument suffix = new StringArgument("s", "suffix", "output file suffix, e.g. .paf.bgz", false);
+        ArgumentList arguments = new ArgumentList(about, help, inputFile, bytes, records, prefix, leftPad, suffix);
+        CommandLine commandLine = new CommandLine(args);
+
+        SplitPaf splitPaf = null;
+        try
+        {
+            CommandLineParser.parse(commandLine, arguments);
+            if (about.wasFound()) {
+                About.about(System.out);
+                System.exit(0);
+            }
+            if (help.wasFound()) {
+                Usage.usage(USAGE, null, commandLine, arguments, System.out);
+                System.exit(0);
+            }
+
+            Long b = bytes.wasFound() ? toBytes(bytes.getValue()) : null;
+
+            String p = prefix.getValue();
+            if (!prefix.wasFound()) {
+                if (inputFile.wasFound()) {
+                    p = getBaseName(inputFile.getValue());
+                }
+                else {
+                    p = "x";
+                }
+            }
+
+            String s = suffix.getValue();
+            if (!suffix.wasFound()) {
+                if (inputFile.wasFound()) {
+                    s = getFileExtensions(inputFile.getValue());
+                }
+                else {
+                    if (Compress.isBgzfInputStream(System.in)) {
+                        s = ".paf.bgz";
+                    }
+                    else if (Compress.isGzipInputStream(System.in)) {
+                        s = ".paf.gz";
+                    }
+                    else if (Compress.isBzip2InputStream(System.in)) {
+                        s = ".paf.bz2";
+                    }
+                    else {
+                        s = ".paf";
+                    }
+                }
+            }
+
+            splitPaf = new SplitPaf(inputFile.getValue(), b, records.getValue(), p, leftPad.getValue(-1), s);
+        }
+        catch (CommandLineParseException | NullPointerException e) {
+            if (about.wasFound()) {
+                About.about(System.out);
+                System.exit(0);
+            }
+            if (help.wasFound()) {
+                Usage.usage(USAGE, null, commandLine, arguments, System.out);
+                System.exit(0);
+            }
+            Usage.usage(USAGE, e, commandLine, arguments, System.err);
+            System.exit(-1);
+        }
+        try {
+            System.exit(splitPaf.call());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
