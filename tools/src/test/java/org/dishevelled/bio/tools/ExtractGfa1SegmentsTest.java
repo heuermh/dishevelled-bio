8556e17f3b45c200356a7c917373c1ea63bdbeb2
Michael L Heuer <heuermh@acm.org>
2021-07-23 10:17:08 -0500

    improve unit tests

--- tools/src/test/java/org/dishevelled/bio/tools/ExtractGfa1SegmentsTest.java
+++ tools/src/test/java/org/dishevelled/bio/tools/ExtractGfa1SegmentsTest.java
@@ -23,10 +23,19 @@
 */
 package org.dishevelled.bio.tools;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
+import java.io.IOException;
 
+import java.nio.charset.Charset;
+
+import com.google.common.io.Files;
+import com.google.common.io.Resources;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -38,8 +47,33 @@ public final class ExtractGfa1SegmentsTest {
     private File inputGfa1File;
     private File outputFastaFile;
 
+    @Before
+    public void setUp() throws IOException {
+        inputGfa1File = File.createTempFile("extractGfa1SegmentsTest", ".gfa");
+        outputFastaFile = File.createTempFile("extractGfa1SegmentsTest", "fa");
+    }
+
+    @After
+    public void tearDown() {
+        inputGfa1File.delete();
+        outputFastaFile.delete();
+    }
+
     @Test
     public void testConstructor() {
         assertNotNull(new ExtractGfa1Segments(inputGfa1File, outputFastaFile, ExtractGfa1Segments.DEFAULT_LINE_WIDTH));
+    }
+
+    @Test
+    public void testExtractGfa1Segments() throws Exception {
+        copyResource("segments.gfa", inputGfa1File);
+        new ExtractGfa1Segments(inputGfa1File, outputFastaFile, ExtractGfa1Segments.DEFAULT_LINE_WIDTH).call();
+
+        String description = Files.asCharSource(outputFastaFile, Charset.forName("UTF-8")).readFirstLine();
+        assertEquals(">2 LN:i:2\tRC:i:50\tFC:i:100\tKC:i:0\tzz:Z:Test", description);
+    }
+
+    private static void copyResource(final String name, final File file) throws Exception {
+        Files.write(Resources.toByteArray(ExtractGfa1SegmentsTest.class.getResource(name)), file);
     }
 }
